df8141f9c30923066efe9b4d4dc215bdcc3db667
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-05-09 11:58:31 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({message: "Hello!"}); //o método send permite enviar uma msg para quem esta requisitando 
+app.get("/courses", (request, response) => {
+    return response.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 3"
+    ]); //o método send permite enviar uma msg para quem esta requisitando 
 })
 
 //para o express funcionar precisamos definir uma porta onde a nosssa aplicação vai estar rodando
-app.listen(3333) //startando a nossa aplicação
+app.listen(3333); //startando a nossa aplicação
 
-//na maioria das vezes vamos usar a função de json em vez de send() porque os retornos vão ser no formato json
+//na maioria das vezes vamos usar a função de json em vez do send pq os retornos são no formato de json
