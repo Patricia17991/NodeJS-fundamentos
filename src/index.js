27866e3e51ff9ce4e29a16a5fbbae2038abfae08
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-05-09 09:04:57 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.get("/", (request, response) => {
+    return response.send("Hello World!"); //o método send permite enviar uma msg para quem esta requisitando 
+})
+
 //para o express funcionar precisamos definir uma porta onde a nosssa aplicação vai estar rodando
 app.listen(3333) //startando a nossa aplicação
