4559ee85ee5b93bd156021fc363c9e5349350e51
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-05-09 08:58:24 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ const express = require('express');
 const app = express();
 
 //para o express funcionar precisamos definir uma porta onde a nosssa aplicação vai estar rodando
-app.listen() //startando a nossa aplicação
+app.listen(3333) //startando a nossa aplicação
