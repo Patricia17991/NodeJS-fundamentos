097782023638e69da122c0e3cc0717605bfe42fc
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-05-09 08:57:26 -0300

    Update and rename index.html to src/index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+const app = express();
+
+//para o express funcionar precisamos definir uma porta onde a nosssa aplicação vai estar rodando
+app.listen() //startando a nossa aplicação
